4aafa6a4d9e82f767e47fb76adf76d69a7b2f5b8
Onur Caglayan <62426634+onurmx@users.noreply.github.com>
2021-05-18 19:36:45 +0300

    Size Converter

---
+++ Mini-Paint/Converters/SizeConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace Mini_Paint
+{
+    public class SizeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                return value;
+            }
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (((string)value).All(x => char.IsNumber(x)) && ((string)value) != "")
+            {
+                return Int32.Parse((string)value);
+            }
+            return 0;
+        }
+    }
+}
